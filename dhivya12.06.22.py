099083830a69c51083ee0b214ec0690e6d036795
dhivyadharshini2709 <107019938+dhivyadharshini2709@users.noreply.github.com>
2022-06-12 22:09:21 +0530

    Add files via upload

---
+++ dhivya12.06.22.py
@@ -0,0 +1,12 @@
+name="dhivyadharshini"
+address="no:13 pillaiyar koil street,kamaraj nagar"
+age="17"
+father="gajapathy"
+mother="umamaheswari"
+school="St.Joseph of Cluny"
+print("student name:",name)
+print("student address:",address)
+print("student age:",age)
+print("father's name:",father)
+print("mother's name:",mother)
+print("educational institution:",school)
